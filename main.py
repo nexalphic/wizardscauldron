26f3d94e34e1a9453a2da5593d121eb821cc87d9
nexalphic <nathangarces1@gmail.com>
2017-02-08 14:30:44 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import math
+import time
+
+print "nathan garces"
+time.sleep(1)
+print
+print "a game of meddling with the laws of physics and magic:"
+time.sleep(1)
+print
+print "the wizard's cauldron"
+
+result = []
+
+def main()
+    print "you are a apprentice of the arcane arts"
+    time.sleep(1)
+    print
+    print "customer asks for a surprise"
+    time.sleep(1)
+    print
+    print "make somerthing using the cauldron"
+    print "cauldron is full of boiling clear water from the stream"
+    choice_1 = first_ingredient()
+    print
+    if choice_1 = 1:
+        print "souls of the dead can be faintly heard"
+    elif choice_1 = 2:
+        print "blue arcane runes are clustering in the cauldron"
+    else:
+        print "smells like a nice forest and weed"
+    
+def first_ingredient():
+    first_choice = int(input("""the ingredients are stored on various shelves
+    					   1:dead man's tooth
+                           2:runic iron
+                           3:pure leaf"""))
+    return first_choice
+    
